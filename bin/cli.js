6342e90d45b00531f359408604cd951984c66a78
wangjin <wangjin@imdada.cn>
2019-05-15 02:09:05 +0800

    [U] update qshell qupload2

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-
+const serviceVersion = require('git-rev-sync').short()
 const path = require('path')
 const manifest = require('../manifest.json')
 const { spawn } = require('child_process')
+const config = require(path.join(__dirname,'../../../package.json'))
 
 const bin = path.join(
   __dirname,
@@ -11,6 +12,34 @@ const bin = path.join(
   manifest.filename[process.platform][process.arch]
 )
 
-const args = process.argv.slice(2)
+let args = process.argv.slice(2)
+
+if (args[0] === 'qupload2') {
+  const attrMap = {
+    '--src-dir': 'build',
+    '--bucket': 'dada-fe',
+    '--key-prefix': `${config.name}/${serviceVersion}/`,
+    '--rescan-local': true,
+    '--skip-suffixes': '.html,.json,.map',
+  }
+
+  const rest = args.slice(1)
+
+  rest.forEach(each => {
+    const split = each.split('=')
+    attrMap[split[0]] = split[1]
+  })
+
+  args = Object.keys(attrMap).reduce(
+    (finalArgs, each) => {
+      finalArgs.push(`${each}=${attrMap[each]}`)
+      return finalArgs
+    },
+    ['qupload2']
+  )
+}
+
+// example
+// qshell qupload2 --src-dir=build --bucket=dada-fe --key-prefix=goose/commit_hash --rescan-local=true --skip-suffixes=.html,.json
 
 spawn(bin, args, { stdio: 'inherit' })
